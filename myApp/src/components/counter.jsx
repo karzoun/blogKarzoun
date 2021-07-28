6df50954a4cedbfb785021e52b696bd8363ec3ba
“karzoun” <karzoun_27@hotmail.com>
2021-07-27 20:43:51 -0700

    adding tags

--- myApp/src/components/counter.jsx
+++ myApp/src/components/counter.jsx
@@ -3,20 +3,20 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
     //imageUrl: "https://picsum.photos/200",
   };
 
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
-  }
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
 
